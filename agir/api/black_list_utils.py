aafbe36aefd70ea3b786e29ba96a516bde2d7f25
Alexandra <bug.graph@gmail.com>
2025-02-21 11:59:54 +0100

    chore: log black listed triggered

--- agir/api/black_list_utils.py
+++ agir/api/black_list_utils.py
@@ -1,15 +1,20 @@
 from agir.api.settings import BLACK_LIST_DF
 from django.core.exceptions import ValidationError
 
+import logging
+logger = logging.getLogger(__name__)
+
 
 class BlackListFieldMixin:
     def clean(self):
         class_name = self.__class__.__name__
         for field in self._meta.fields:
             field_name = field.name
+            value = str(getattr(self, field_name, "")).lower()
             if not field_allowed(
-                class_name, field_name, str(getattr(self, field_name, "")).lower()
+                class_name, field_name, value
             ):
+                logger.warning(f"{value} not allowed for {class_name}.{field_name}")
                 raise ValidationError(
                     "Une erreur s'est produite lors de l'enregistrement.",
                     code="wrong",
